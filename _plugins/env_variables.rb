1b9c3abef94ae2fb0ead89d59362de404135c22b
St. Mary's MSOC Norway <stmaryschurchnorway@gmail.com>
2026-05-07 19:34:55 +0200

    debug: add env var listing to plugin for troubleshooting

--- _plugins/env_variables.rb
+++ _plugins/env_variables.rb
@@ -10,6 +10,7 @@ Jekyll::Hooks.register :site, :post_read do |site|
   Jekyll.logger.info "[DEBUG] GOOGLE_API_KEY env var exists: #{!ENV['GOOGLE_API_KEY'].nil?}"
   Jekyll.logger.info "[DEBUG] GOOGLE_API_KEY value length: #{google_api_key.length}"
   Jekyll.logger.info "[DEBUG] JEKYLL_ENV: #{ENV['JEKYLL_ENV']}"
+  Jekyll.logger.info "[DEBUG] All env vars: #{ENV.keys.sort.join(', ')}"
 
   # Set it in the Jekyll config so it's available in templates as {{ site.google_api_key }}
   site.config['google_api_key'] = google_api_key
